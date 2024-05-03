de097859310a0f9bff0742bce5c9faf8dea640bc
daande01 <daniel.evert.andersson@gmail.com>
2024-05-03 08:36:54 +0200

    new file

--- prog1_indval23/src/egengrafik/Pacman.java
+++ prog1_indval23/src/egengrafik/Pacman.java
@@ -4,18 +4,40 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
-public class Pacman extends JComponent {
+public class Pacman extends JComponent implements KeyListener {
 
+	int x= 100;
+	int y=100;
 
 	int pollyx[]= {150,100,100};
 	int pollyy[]= {150,140,175};
+	Timer t ;
 	
 	public Pacman() {
 			this.setPreferredSize(new Dimension(400,400));
+	
+	t=new Timer(1000,e->{
+		
+		
+		if(pollyy[1]==175) {
+		pollyy[1]=140;
+		}else {
+			pollyy[1]=175;
+			
+		}
+		repaint();
+		
+		
+	});
+	t.start();
+	
 	}
 	
 	
@@ -25,7 +47,7 @@ public class Pacman extends JComponent {
 		super.paintComponent(g);
 
 		g.setColor(Color.YELLOW);
-		g.fillOval(100, 100, 100, 100);
+		g.fillOval(x,y, 100, 100);
 		g.setColor(this.getBackground());
 		g.fillPolygon(pollyx, pollyy, 3);
 		
@@ -34,12 +56,43 @@ public class Pacman extends JComponent {
 	
 	public static void main(String[] args) {
 		
+		
+		Pacman p = new Pacman();
 		JFrame f = new JFrame();
 		f.setVisible(true);
 		f.setLayout(new FlowLayout());
-		f.add(new Pacman());
+		f.add(p);
 		f.pack();
+		f.addKeyListener(p);
 		
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			
+			x=x+10;
+			repaint();
+			
+		}
+		
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
